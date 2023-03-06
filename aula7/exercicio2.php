8a9579e3a339b872eda5f60486ca01c93a3632db
Nathalia de Oliveira Alaion Cavini <16616687635@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-03-06 08:10:45 -0300

    exercicio aula 7

--- aula7/exercicio2.php
+++ aula7/exercicio2.php
@@ -17,21 +17,15 @@
     </pre>
 </p>
 <p>
-    Imprima os nomes do vetor em formato de lista ordenada, utilizando um laço de repetição: ex:
-        <br>
-        1. Murilo <br>
-        2. Poliana <br>
-        3. .... <br>
+    Imprima os nomes do vetor em formato de lista ordenada, utilizando um laço de repetição:
 </p>
 
 <?php
-    $nomes[0] = "Murilo";
-    $nomes[1] = "Poliana";
-    $nomes[2] = "Dalva";
-    $nomes[3] = "Lavinia";
-    $nomes[4] = "Kaua";
+    $nomes = ["Murilo", "Poliana", "Dalva", "Lavinia", "Kaua"];
     
-    for($contador=0; $contador=4; $contador++){
-        echo "<>";
+      echo "<ol>";
+      for($contador=0; $contador<=4; $contador++){
+      echo "<li> $nomes[$contador] </li>";
     }
+      echo "</ol>";
 ?>
---
+++ aula8/exercicio1.php
@@ -0,0 +1,13 @@
+<h2>Ordenação - Ordem Alfabética</h2>
+
+<?php
+
+    $nomes = ["Murilo", "Poliana", "Dalva", "Lavinia", "Camila", "Kaua"];
+
+    if($nomes[0] < $nomes[1]){
+        
+    }
+
+    //Camila, Dalva, Kaua, Lavinia, Murilo, Poliana
+
+?>
