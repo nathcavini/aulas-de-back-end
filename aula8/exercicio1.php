5387e531adb1a37b133bf5a08a0dcdc54fec6da7
Nathalia de Oliveira Alaion Cavini <16616687635@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-03-06 08:46:55 -0300

    Exercicio Ordenação

--- aula8/exercicio1.php
+++ aula8/exercicio1.php
@@ -4,9 +4,19 @@
 
     $nomes = ["Murilo", "Poliana", "Dalva", "Lavinia", "Camila", "Kaua"];
 
-    if($nomes[0] < $nomes[1]){
-        
+    //Algoritmo Select Sort
+
+    for($c = 0; $c<=4; $c++){
+        for($i = $c + 1; $i<=5; $i++){
+            if($nomes[$c] > $nomes[$i]){
+                $celBranco = $nomes[$i];
+                $nomes[$i] = $nomes[$c];
+                $nomes[$c] = $celBranco;
+            }
+        }
     }
+
+    var_dump($nomes);
 
     //Camila, Dalva, Kaua, Lavinia, Murilo, Poliana
 
